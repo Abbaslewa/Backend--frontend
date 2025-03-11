a058cb2d58662b7d59f38dae2ea745440c31b95e
AbbasLewa <abbasomerhassn@gmail.com>
2025-03-11 16:21:10 +0200

    updat

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://full-backend-gxdc.onrender.com/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
@@ -29,7 +29,6 @@ const CreateBooks = () => {
       })
       .catch((error) => {
         setLoading(false);
-        // alert('An error happened. Please Chack console');
         enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
@@ -38,42 +37,42 @@ const CreateBooks = () => {
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+      <h1 className='text-4xl my-6 text-blue-950'>Create Book</h1>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='flex flex-col border-2 border-blue-950 rounded-xl w-[600px] p-6 mx-auto bg-gradient-to-r from-blue-100 to-blue-50'>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <label className='text-xl mr-4 text-blue-950'>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-950'
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <label className='text-xl mr-4 text-blue-950'>Author</label>
           <input
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-950'
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <label className='text-xl mr-4 text-blue-950'>Publish Year</label>
           <input
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-900'
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className='p-3 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition-all duration-200' onClick={handleSaveBook}>
           Save
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default CreateBooks
+export default CreateBooks;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+import { MdOutlineAddBox, } from 'react-icons/md';
 import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
 
@@ -16,9 +15,9 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books') // Ensure this URL is correct
+      .get('https://full-backend-gxdc.onrender.com/books') 
       .then((response) => {
-        setBooks(response.data.data || response.data); // Adjust as per API response
+        setBooks(response.data.data || response.data);
         setLoading(false);
       })
       .catch((error) => {
@@ -28,29 +27,33 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
+    <div className='bg-gray-50 min-h-screen p-8'>
+      <div className='flex justify-center items-center gap-x-6 mb-6'>
         <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          className='bg-sky-500 text-white hover:bg-sky-700 px-6 py-2 rounded-md transition-colors duration-300'
           onClick={() => setShowType('table')}
         >
-          Table
+          Table View
         </button>
         <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          className='bg-sky-500 text-white hover:bg-sky-700 px-6 py-2 rounded-md transition-colors duration-300'
           onClick={() => setShowType('card')}
         >
-          Card
+          Card View
         </button>
       </div>
+      
       <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
+        <h1 className='text-4xl font-semibold text-gray-800 mb-8'>Books List</h1>
         <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+          <MdOutlineAddBox className='text-sky-800 text-5xl hover:text-sky-600 transition-colors duration-300 cursor-pointer' />
         </Link>
       </div>
+
       {loading ? (
-        <Spinner />
+        <div className='flex justify-center items-center'>
+          <Spinner />
+        </div>
       ) : showType === 'table' ? (
         <BooksTable books={books} />
       ) : (
